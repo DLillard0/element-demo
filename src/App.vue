bdd6da4233241a72f5e35f43a5d35b491c8e0860
yuanjunkai <yuanjunkai@linklogis.com>
2023-08-02 15:33:05 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <date-picker
+        v-model="value1"
+        type="yearrange"
+        range-separator="至"
+        start-placeholder="开始年份"
+        end-placeholder="结束年份">
+      </date-picker>
+    </div>
+    <div class="block">
+      <span class="demonstration">带快捷选项</span>
+      <date-picker
+        v-model="value2"
+        type="yearrange"
+        align="right"
+        unlink-panels
+        range-separator="至"
+        start-placeholder="开始年份"
+        end-placeholder="结束年份"
+        :picker-options="pickerOptions">
+      </date-picker>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DatePicker from './components/date-picker'
+import './components/date-picker.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DatePicker
+  },
+  data() {
+    return {
+      pickerOptions: {
+        shortcuts: [{
+          text: '今年',
+          onClick(picker) {
+            picker.$emit('pick', [new Date(), new Date()])
+          }
+        }, {
+          text: '2000年至今',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date(new Date('2000').getFullYear(), 0)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      },
+      value1: '',
+      value2: ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
